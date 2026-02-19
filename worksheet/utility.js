969a82c4ebe94e63540095f42304d3694a54467b
Luke Linington <lukelinington@gmail.com>
2026-02-19 22:44:45 +0000

    export functions for compare, largest, and zeroest in utility.js

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,35 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+export function compare(a, b){
+  if (a.length != b.length) return false 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) return false  
+    
+  }
+  return true 
+
+}
+
+export function largest (arr){
+  let largestValue = arr[0]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]>largestValue) largestValue = arr[i]
+    
+  }
+  return largestValue
+}
+
+export function zeroest(arr){
+  let zerostValue = arr[0]
+  const largestValue = largest(arr)
+  for (let i = 0; i < arr.length; i++) {
+    if (largestValue - Math.abs(arr[i])> largestValue-Math.abs(zerostValue)) zerostValue = arr[i]
+    
+  }
+  return zerostValue
 }
